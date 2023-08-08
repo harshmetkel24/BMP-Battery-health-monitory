a158db51537db4a3be02caad13d94328fe8b06de
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-08-09 02:12:44 +0530

    test2

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ function downloadCSV(adcTime, filename) {
 
   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
 
-  const link = document.querySelector("download-btn");
+  const link = document.querySelector(".download-btn");
   link.href = URL.createObjectURL(blob);
   link.download = filename;
 

df1b35891e18b8dcee61fa04bb76e19951a43a78
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-10-26 22:58:34 +0530

    dynamic home page form created

--- frontend/public/scripts/home.js
+++ frontend/public/scripts/home.js
@@ -5,3 +5,45 @@ window.addEventListener("load", () => {
     document.querySelector(".container-fluid").style.opacity = 1; // Fade in the content
   }, 1500);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  let inputCount = 1;
+
+  // Function to create a new input field
+  function createNewInput() {
+    inputCount++;
+    const newInput = document.createElement("div");
+    newInput.className = "form-group mb-3 input-group dynamic-input";
+    newInput.innerHTML = `
+      <div class="input-group mb-3 border border-2 border-dark rounded-3">
+        <span class="input-group-text text-primary" id="basic-addon1">Battery ${inputCount}</span>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter Battery link"
+          name="battery${inputCount}"
+        />
+        <span class="input-group-text text-danger" id="basic-addon2">
+          <i class="fa-solid fa-trash remove-input"></i>
+        </span>
+      </div>
+    `;
+    return newInput;
+  }
+
+  // Function to add a new input field when "Add Battery" is clicked
+  document.querySelector("#add-battery").addEventListener("click", function () {
+    const newInput = createNewInput();
+    const addBatteryButton = document.getElementById("add-battery");
+    addBatteryButton.parentElement.previousElementSibling.appendChild(newInput);
+    console.log(addBatteryButton.parentElement.previousElementSibling)
+
+    // Add an event listener to the new input's trash icon for removal
+    const trashIcon = newInput.querySelector(".remove-input");
+    trashIcon.addEventListener("click", function () {
+      newInput.remove();
+    });
+  });
+
+  // cant remove inital trash icon
+});
